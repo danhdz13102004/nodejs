b230d026b574f1a9f036f203887ec8f8deb4ce44
danhdz13102004 <danh0813449262@gmail.com>
2025-04-18 21:44:12 +0700

    edit env

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect("mongodb+srv://danh:danh@cluster0.udfhbjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection failed:', err));
 
